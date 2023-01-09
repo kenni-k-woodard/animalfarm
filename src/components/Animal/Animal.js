6080c8bdf2b456141b511584bae1e7c473c7e82b
Kenni Woodard <kennikristine@gmail.com>
2023-01-09 15:20:56 -0800

    animals and names displaying on farm

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,3 +1,8 @@
-export default function Animal() {
-  return <></>;
+export default function Animal(props) {
+  return (
+    <div className="animal" style={{ top: props.top, left: props.left }}>
+      <img src={`/animals/${props.type}.svg`} />
+      <p>{props.name}</p>
+    </div>
+  );
 }
