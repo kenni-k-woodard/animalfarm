ae164ea44342f09b48e24ad0b78f461a6b1ce448
Kenni Woodard <kennikristine@gmail.com>
2023-01-09 16:43:17 -0800

    things displaying in correct area

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,8 +1,11 @@
+import './Animal.css';
+
 export default function Animal(props) {
   return (
     <div className="animal" style={{ top: props.top, left: props.left }}>
       <img src={`/animals/${props.type}.svg`} />
-      <p>{props.name}</p>
+      <p className="name">{props.name}</p>
+      <p>{props.says}</p>
     </div>
   );
 }
